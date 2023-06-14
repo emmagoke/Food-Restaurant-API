52ba940773178a5654d81c99a991f0b4597eb296
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-14 10:05:25 +0100

    Added Category and MenuItem Serializers

---
+++ FoodRestaurantAPI/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from django.contrib.auth.models import User
+from decimal import Decimal
+
+from .models import Category, MenuItem, Cart, Order, OrderItem
+
+
+class CategorySerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'slug']
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    # category = CategorySerializer(read_only=True)
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'category', 'featured']
