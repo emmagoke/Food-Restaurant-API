24582becc0d361551f17c0be61cb92431e639b47
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-14 10:17:39 +0100

    Added Categories View

--- FoodRestaurantAPI/views.py
+++ FoodRestaurantAPI/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Category, MenuItem, Cart, Order, OrderItem
+from .serializers import CategorySerializer, MenuItemSerializer, CartSerializer, OrderSerializer, UserSerilializer
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.permissions import IsAdminUser
+from django.shortcuts import  get_object_or_404
+
+from django.contrib.auth.models import Group, User
+
+from rest_framework import viewsets
+from rest_framework import status
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
